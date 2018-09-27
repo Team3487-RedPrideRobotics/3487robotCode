35d2c3600ac38f1ea71bb02a7a58e848c21c57dd
bwest328 <43620421+bwest328@users.noreply.github.com>
2018-09-27 08:20:52 -0400

    Removed demo text

--- OffSeason2018/src/main/java/frc/robot/Robot.java
+++ OffSeason2018/src/main/java/frc/robot/Robot.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
@@ -12,24 +5,20 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-/**
- * This is a demo program showing the use of the RobotDrive class, specifically
- * it contains the code necessary to operate a robot with tank drive.
- */
 public class Robot extends IterativeRobot {
-  private DifferentialDrive m_myRobot;
-  private Joystick m_leftStick;
-  private Joystick m_rightStick;
+  private DifferentialDrive myRobot;
+  private Joystick leftStick;
+  private Joystick rightStick;
 
   @Override
   public void robotInit() {
-    m_myRobot = new DifferentialDrive(new Spark(0), new Spark(1));
-    m_leftStick = new Joystick(0);
-    m_rightStick = new Joystick(1);
+    myRobot = new DifferentialDrive(new Spark(0), new Spark(1));
+    leftStick = new Joystick(0);
+    rightStick = new Joystick(1);
   }
 
   @Override
   public void teleopPeriodic() {
-    m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
+    myRobot.tankDrive(leftStick.getY(), rightStick.getY());
   }
 }

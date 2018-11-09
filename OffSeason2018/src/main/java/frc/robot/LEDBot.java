716b318cb25be4544d2f034104d5e35ed7af4d86
Jacob J. Armstrong <jarmstrong7422@redpride.net>
2018-11-09 12:40:37 -0500

    Styling

--- OffSeason2018/src/main/java/frc/robot/LEDBot.java
+++ OffSeason2018/src/main/java/frc/robot/LEDBot.java
@@ -19,29 +19,29 @@ public class LEDBot extends IterativeRobot {
     RevDigitDisplay display = RevDigitDisplay.getInstance();
     display.setActiveModule(new BatteryPercentModule(12D));
 
-     ledController = new SerialPort(115200, Port.kOnboard);
-     byte[] dataOut = new byte[14];
-     Arrays.fill(dataOut, (byte)0x00);
-     dataOut[0] = (byte)0xAA;
-     dataOut[1] = (byte)0xff;
-     dataOut[2] = (byte)0xff;
+    ledController = new SerialPort(115200, Port.kOnboard);
+    byte[] dataOut = new byte[14];
+    Arrays.fill(dataOut, (byte)0x00);
+    dataOut[0] = (byte)0xAA;
+    dataOut[1] = (byte)0xff;
+    dataOut[2] = (byte)0xff;
      
-     short checkSum = 0;
-     for(short i =0; i <13;i++) {
+    short checkSum = 0;
+    for(short i =0; i <13;i++) {
 
-       checkSum += dataOut[i];
+      checkSum += dataOut[i];
 
-     }
+    }
+
      dataOut[13] = (byte) checkSum;
 
-     for(int i=0;i<13;i++) {
+    for(int i=0;i<13;i++) {
 
       dataOut[i] =(byte) ~ dataOut[i];
 
-     }
+    }
 
-     ledController.write(dataOut, 14);
-
+    ledController.write(dataOut, 14);
 
   }
 

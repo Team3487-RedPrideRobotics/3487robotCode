3be4304bb972d543400f947fcde75bc954c994dc
Jacob J. Armstrong <jarmstrong7422@redpride.net>
2018-11-09 07:59:41 -0500

    Attempting Checksum

--- OffSeason2018/src/main/java/frc/robot/LEDBot.java
+++ OffSeason2018/src/main/java/frc/robot/LEDBot.java
@@ -1,17 +1,34 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.SerialPort.Port;
+import java.util.Arrays;
 
 public class LEDBot extends IterativeRobot {
 
   //RobotController.getBatteryVoltage();
+  SerialPort ledController;
 
   @Override
   public void robotInit() {
 
+     ledController = new SerialPort(115200, Port.kOnboard);
+     byte[] dataOut = new byte[14];
+     Arrays.fill(dataOut, (byte)0x00);
+     dataOut[0] = (byte)0xAA;
+     dataOut[1] = (byte)0xff;
+     dataOut[2] = (byte)0xff;
+     
+     short checkSum = 0;
+     for(short i =0; i <13;i++) {
+
+       checkSum += dataOut[i];
+
+     }
+     dataOut[13] = (byte) checkSum;
+
+     ledController.write(dataOut, 14);
 
 
   }
@@ -19,7 +36,7 @@ public class LEDBot extends IterativeRobot {
   @Override
   public void teleopPeriodic() {
 
-s
+
 
   }
   

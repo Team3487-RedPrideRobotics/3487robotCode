2b74c3b0aa1ac79a33ae6aae616cc12e40e99a3a
Jake Armstrong <Lainkai@users.noreply.github.com>
2018-11-25 13:24:18 -0500

    LED Drivers added

--- OffSeason2018/src/main/java/frc/robot/LEDBot.java
+++ OffSeason2018/src/main/java/frc/robot/LEDBot.java
@@ -1,25 +1,27 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Spark;
-
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SerialPort.Port;
-import java.util.Arrays;
-
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import net.bak3dnet.robotics.displays.RevDigitDisplay;
-import net.bak3dnet.robotics.displays.modules.BatteryPercentModule;;
+import net.bak3dnet.robotics.displays.modules.BatteryPercentModule;
+
+import net.bak3dnet.robotics.led.LightDrive12;
+import net.bak3dnet.robotics.led.modules.AGradientModule;
+import net.bak3dnet.robotics.led.modules.util.GradientMap;
+import net.bak3dnet.robotics.led.Color;
 
 public class LEDBot extends IterativeRobot {
 
-  //RobotController.getBatteryVoltage();
+  // RobotController.getBatteryVoltage();
   SerialPort ledController;
 
   DifferentialDrive myRobot;
   Joystick leftStick;
-  Joystick  rightStick;
+  Joystick rightStick;
 
   @Override
   public void robotInit() {
@@ -31,6 +33,7 @@ public class LEDBot extends IterativeRobot {
     RevDigitDisplay display = RevDigitDisplay.getInstance();
     display.setActiveModule(new BatteryPercentModule(12D));
 
+    /*
     //Led Controller Test Code
     ledController = new SerialPort(115200, Port.kOnboard);
     ledController.setWriteBufferMode(SerialPort.WriteBufferMode.kFlushOnAccess);
@@ -53,7 +56,19 @@ public class LEDBot extends IterativeRobot {
     dataOut[13] = checkSum;
 
     ledController.write(dataOut, 14);
+*/
 
+    LightDrive12 ld12 = new LightDrive12(Port.kOnboard);
+    GradientMap colors = new GradientMap(new Color("F00"),0L);
+    colors.put(new Color("FF0"), 150L);
+    colors.put(new Color("0F0"), 150L);
+    colors.put(new Color("0Ff"), 150L);
+    colors.put(new Color("00f"), 150L);
+    colors.put(new Color("F0F"), 150L);
+    colors.put(new Color("F00"), 150L);
+
+    ld12.setChannelModule(new AGradientModule(colors));
+  
   }
 
   @Override
